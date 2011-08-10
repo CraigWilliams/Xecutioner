491510d44812c1acc7d6b16d0a987276d9c02ca6
Craig Williams <cwilliams@allancraig.net>
2011-08-10 14:08:21 -0600

    also find 'it' statements for RSpec

--- parse_file.rb
+++ parse_file.rb
@@ -16,9 +16,17 @@ found_lines = []
 line_count = 1
 
 while (line = file.gets)
-  found_lines << "#{line.gsub(/('|"|do)/,'').strip}:#{line_count}" if line =~ /(describe|context|scenario)/i
+  found_lines << "#{line.gsub(/('|"|do)/,'').strip}:#{line_count}" if line =~ /^\s+?(describe|context|scenario|it)/i
   line_count += 1
 end
 
-print found_lines.join("\n")
+formatted_lines = []
+found_lines.each do |line|
+  if line =~ /^\s+?it/
+    line = "   #{line}"
+  end
+  formatted_lines << line
+end
+
+print formatted_lines.join("\n")
 
