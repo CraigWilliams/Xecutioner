68e98ad6df7672d90f85a3557fed364989a83f62
Craig Williams <cwilliams.allancraig@gmail.com>
2012-10-14 11:45:03 -0600

    Rename parse_file to parse

--- parse.rb
+++ parse.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def pad(number)
-  "%03d" % number
+  "%03s" % number
 end
 
 if ARGV.size == 0
@@ -13,12 +13,13 @@ found_lines     = []
 line_count      = 1
 file_path       = ARGV[0]
 file_path       = File.expand_path(file_path) if file_path =~ /^~/
-file            = File.open(file_path, 'r')
 formatted_lines = []
 
-while (line = file.gets)
-  found_lines << "#{pad(line_count)}:#{line.gsub(/('|"|do)/,'').strip}" if line =~ /^\s+?(describe|context|scenario|it)/i
-  line_count += 1
+File.open(file_path, 'r') do |file|
+  while line = file.gets
+    found_lines << "#{line_count}:#{line.gsub(/('|"|do)/,'').strip}" if line =~ /^\s+?(describe|context|scenario|it)/i
+    line_count += 1
+  end
 end
 
 found_lines.each do |line|
